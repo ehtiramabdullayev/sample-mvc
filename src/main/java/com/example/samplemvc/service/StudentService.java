0896610b3d1ec694a3fb02a71ddba5bdcf153244
Master <ehtiramabdullayev@gmail.com>
2024-04-30 00:28:13 +0200

    adding archunit tests

--- src/main/java/com/example/samplemvc/service/StudentService.java
+++ src/main/java/com/example/samplemvc/service/StudentService.java
@@ -10,6 +10,7 @@ import java.util.Optional;
 
 @Service
 public class StudentService {
+
     @Autowired
     private StudentRepository studentRepository;
 
---
+++ src/test/java/com/example/samplemvc/ArchitectureTest.java
@@ -0,0 +1,59 @@
+package com.example.samplemvc;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.junit.AnalyzeClasses;
+import com.tngtech.archunit.lang.ArchRule;
+import com.tngtech.archunit.lang.syntax.ArchRuleDefinition;
+import org.junit.jupiter.api.Test;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+import static com.tngtech.archunit.library.Architectures.layeredArchitecture;
+import static com.tngtech.archunit.library.GeneralCodingRules.ACCESS_STANDARD_STREAMS;
+import static com.tngtech.archunit.library.GeneralCodingRules.USE_JAVA_UTIL_LOGGING;
+
+@AnalyzeClasses(
+        packages = "com.example.samplemvc"
+)
+public class ArchitectureTest {
+
+    private final JavaClasses importedClasses = new ClassFileImporter().importPackages("com.example.samplemvc");
+
+
+    @Test
+    public void testLayeredArchitecture() {
+
+        layeredArchitecture()
+                .consideringOnlyDependenciesInLayers()
+                .layer("Controllers").definedBy("..controller..")
+                .layer("Services").definedBy("..service..")
+                .layer("Repositories").definedBy("..repository..")
+                .whereLayer("Services").mayOnlyBeAccessedByLayers("Controllers")
+                .whereLayer("Repositories").mayOnlyBeAccessedByLayers("Services")
+                .check(importedClasses);
+
+    }
+
+
+    @Test
+    public void testServiceClassesShouldBeAnnotatedWithService() {
+        ArchRule serviceClassesShouldBeAnnotated = classes()
+                .that().resideInAPackage("..service..")
+                .should().beAnnotatedWith(Service.class);
+
+        serviceClassesShouldBeAnnotated.check(importedClasses);
+    }
+
+    @Test
+    public void testRepositoryClassesShouldBeAnnotatedWithRepository() {
+        ArchRule repositoryClassesShouldBeAnnotated = classes()
+                .that().resideInAPackage("..repository..")
+                .should().beAnnotatedWith(Repository.class);
+
+        repositoryClassesShouldBeAnnotated.check(importedClasses);
+    }
+
+}
